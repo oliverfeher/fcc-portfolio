809cc3108d9c6c3ec52310d7a50ce35ae253840c
oliverfeher <olcsee@gmail.com>
2019-12-26 02:13:46 -0500

    Updating app.js, index.html and style.css
    - Add background change effect to app.js on mouseover
    - Add new section about to index.html
    - Add img to about-section
    - Add content to about-section bio
    - Add footer to index.html

--- app.js
+++ app.js
@@ -14,29 +14,37 @@ const title4 = document.getElementById("title4");
 /* Project hover functions */
 project1.onmouseover = function() {
     title1.innerHTML = "Click to View!"
+    title1.style.backgroundColor = "#E65C4F";
 }
 project1.onmouseout = function() {
     title1.innerHTML = "Survey Form";
+    title1.style.backgroundColor = "#326789";
 }
 
 project2.onmouseover = function() {
     title2.innerHTML = "Click to View!"
+    title2.style.backgroundColor = "#E65C4F";
 }
 project2.onmouseout = function() {
     title2.innerHTML = "React App";
+    title2.style.backgroundColor = "#326789"
 }
 
 project3.onmouseover = function() {
     title3.innerHTML = "Click to View!"
+    title3.style.backgroundColor = "#E65C4F";
 }
 project3.onmouseout = function() {
     title3.innerHTML = "Hide and Seek";
+    title3.style.backgroundColor = "#326789"
 }
 
 project4.onmouseover = function() {
     title4.innerHTML = "Click to View!"
+    title4.style.backgroundColor = "#E65C4F";
 }
 project4.onmouseout = function() {
     title4.innerHTML = "Landing Page";
+    title4.style.backgroundColor = "#326789"
 }
 
